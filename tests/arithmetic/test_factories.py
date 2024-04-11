b314d03e2c581cc775c99d1ad8e1953cd06ec370
Daniel Morawski <gvrmvnn@gmail.com>
2024-04-11 13:59:32 +0200

    Implement tests and method solve for arithmetic.factories

--- tests/arithmetic/test_factories.py
+++ tests/arithmetic/test_factories.py
@@ -23,11 +23,22 @@ def test_arithmetic_generate(factory):
     #
     for exercise in exercises:
         # Validate operator
-        assert factory.operator.strip() in exercise
+        assert factory.operator_symbol.strip() in exercise
 
         # Check amount of components in exercise
-        components = exercise.split(factory.operator)
+        components = exercise.split(factory.operator_symbol)
         component_count = 3 if level > 3 else 2
         assert len(components) == component_count
         # Validate components
         assert all(component.isdigit() for component in components)
+
+
+@pytest.mark.parametrize("factory, exercises, expected_results", [
+    (AdditionFactory(), ['7712 + 2211 + 2458', '9209 + 5627 + 16'], [12381, 14852]),
+    (SubtractionFactory(), ['58 - 1', '33 - 11', '75 - 64', '62 - 32', '47 - 22'], [57, 22, 11, 30, 25]),
+    (MultiplicationFactory(), ['94 * 742', '5 * 589', '314 * 6'], [69748, 2945, 1884]),
+    (DivisionFactory(), ['256 / 16', '1000 / 10', '333 / 111'], [16, 100, 3])
+])
+def test_arithmetic_solve(factory, exercises, expected_results):
+    assert factory.solve(exercises) == expected_results
+
