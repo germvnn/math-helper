f27d27f383359a22ccb3b26166d2c3e4a4b3617c
Daniel Morawski <gvrmvnn@gmail.com>
2024-04-13 12:34:40 +0200

    A little adjustment to constants

--- PDFGenerator/constants.py
+++ PDFGenerator/constants.py
@@ -1,5 +1,5 @@
 AUTHOR_NAME = "Daniel Morawski"
-PROJECT_NAME = "MathGenerator Library"
+PROJECT_NAME = "Math-Helper"
 
 GITHUB_PROFILE = "https://github.com/germvnn"
 GITHUB_PROJECT_REPOSITORY = "https://github.com/germvnn/math-helper"
