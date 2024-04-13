76b397de16261d4eb7d255d21609aa14f99ea61d
Daniel Morawski <gvrmvnn@gmail.com>
2024-04-13 20:49:50 +0200

    Add some documentation cli.py

--- cli.py
+++ cli.py
@@ -4,8 +4,19 @@ from PDFGenerator.builder import Director, MathPDFBuilder
 
 
 class Factory:
+
     @staticmethod
     def create(operation):
+        """
+        Static method to create a factory object
+        based on the type of arithmetic operation.
+
+        Parameters:
+        - operation (str): The type of arithmetic operation for which the factory is created.
+
+        Returns:
+        - An instance of the requested factory class.
+        """
         factories = {
             'addition': arithmetic.AdditionFactory,
             'subtraction': arithmetic.SubtractionFactory,
@@ -20,6 +31,14 @@ class Factory:
 
 
 def main():
+    """
+    Main function to handle command-line arguments and
+    generate arithmetic exercises along with their solutions.
+
+    It creates exercise and solution PDFs using the specified
+    arithmetic operation, difficulty level, and amount.
+    """
+
     parser = argparse.ArgumentParser(description="Arithmetic Exercise Generator")
     parser.add_argument('-operation', choices=[
         'addition',
@@ -36,18 +55,25 @@ def main():
 
     args = parser.parse_args()
 
+    # Initialize Factory based on passed argument
     factory = Factory.create(operation=args.operation)
+
+    # Generate {args.amount} exercises
     exercises = factory.generate(level=args.level, amount=args.amount)
+
+    # Solve above generated exercises
     solutions = factory.solve(exercises=exercises)
 
+    # Initialize Director and his PDFBuilder
     director = Director()
-    builder = MathPDFBuilder()
-    director.builder = builder
+    director.builder = MathPDFBuilder()
 
+    # Create PDF based on generated exercises
     director.build_exercises(title=f"Exercises from {factory.__class__.__name__}",
                              exercises=exercises,
                              filename="exercises")
 
+    # Create PDF based on solutions of above exercises
     director.build_solutions(title=f"Solutions from {factory.__class__.__name__}",
                              exercises=solutions,
                              filename=f"solutions")
