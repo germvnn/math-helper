120f59231917a9a96f194e7a0149bf93ec76f70c
Daniel Morawski <gvrmvnn@gmail.com>
2024-04-16 16:14:56 +0200

    Implement Quadratic generate method

--- MathGenerator/algebra/quadratic.py
+++ MathGenerator/algebra/quadratic.py
@@ -1,3 +1,6 @@
+import math
+import random
+
 from MathGenerator.abstracts import ExerciseFactory
 
 
@@ -5,10 +8,34 @@ class QuadraticFactory(ExerciseFactory):
     """Class for managing Quadratic exercises"""
     def __init__(self, operator_symbol):
         super().__init__()
-        self.operator_symbol = operator_symbol
+        self.operator_symbol = f" {operator_symbol} "
 
-    def generate(self, level: int, amount: int):
-        pass
+    def generate(self, level, amount):
+        exercises = []
+        # Process of generating single quadratic exercise
+        for _ in range(amount):
+            while True:
+                a = random.randint(-level, level)
+                # Making sure that equation will not be linear
+                while a == 0:
+                    a = random.randint(-level, level)
+
+                b = random.randint(-3 * level, 3 * level)
+
+                # Excluding 'easy' deltas from higher levels
+                if level >= 5:
+                    delta = random.choice([d for d in range(1, level ** 2 + 1) if int(math.sqrt(d)) ** 2 != d])
+                else:
+                    delta = random.choice([d ** 2 for d in range(level, level + 3)])
+
+                c = (b ** 2 - delta) / (4 * a)
+
+                if c.is_integer():
+                    a_str = f"{'-' if a == -1 else '' if a == 1 else a}"
+                    exercises.append(f"{a_str}x^2 + {b}x + {int(c)}{self.operator_symbol}0")
+                    break  # Leave while True if c is an integer
+
+        return exercises
 
     def solve(self, exercise):
         pass
@@ -30,3 +57,10 @@ class QuadraticInequalityGreaterFactory(QuadraticFactory):
     """Quadratic Inequalities (greater)"""
     def __init__(self):
         super().__init__(operator_symbol='>')
+
+
+if __name__ == "__main__":
+    # factory = QuadraticEquationFactory()
+    factory = QuadraticInequalityLessFactory()
+    exe = factory.generate(level=3, amount=10)
+    print(f"exercises: {exe}")
--- tests/algebra/test_quadratic.py
+++ tests/algebra/test_quadratic.py
@@ -1,14 +1,14 @@
-import random
-
 import pytest
+import random
+import re
 
-from MathGenerator.algebra.quadratic import *
+from MathGenerator.algebra import quadratic as q
 
 
 @pytest.mark.parametrize("factory_class, level", [
-    (QuadraticEquationFactory, 2),
-    (QuadraticInequalityLessFactory, 2),
-    (QuadraticInequalityGreaterFactory, 2),
+    (q.QuadraticEquationFactory, 2),
+    (q.QuadraticInequalityLessFactory, 2),
+    (q.QuadraticInequalityGreaterFactory, 2),
 ])
 def test_quadratic_generate(factory_class, level):
     factory = factory_class()
@@ -20,24 +20,28 @@ def test_quadratic_generate(factory_class, level):
     for exercise in exercises:
         assert factory.operator_symbol in exercise, f"Operator {factory.operator_symbol} not found in {exercise}"
 
-        if level > 3:
-            assert 'sqrt' in exercise
         # Assuming the quadratic exercise is structured like 'ax^2 + bx + c operator 0'
-        components = exercise.replace(" ", "").split(factory.operator_symbol)
+        components = exercise.split(factory.operator_symbol)
 
         assert components[1] is '0'
         left_side = components[0]  # Only one side as the other is assumed to be '0'
 
-        # Check components of the quadratic expression
-        assert left_side.count('x^2') == 1 and left_side.count('x') == 1, "Malformed quadratic expression"
+        # Count all occurrences of x
+        all_x = re.findall(r'x', left_side)
+        # Count only x^2
+        x_squared = re.findall(r'x\^2', left_side)
+
+        assert len(x_squared) == 1, "Should be exactly one 'x^2' term."
+        # Since each x^2 includes two x's, adjust the count of single x's
+        assert len(all_x) - len(x_squared) == 1, "Should be exactly one 'x'"
 
 
 @pytest.mark.parametrize("factory_class, test_input, expected_output", [
-    (QuadraticEquationFactory, "x^2 - 5x + 6 = 0",
+    (q.QuadraticEquationFactory, "x^2 - 5x + 6 = 0",
      {"exercise": "x^2 - 5x + 6 = 0", "x1": 2, "x2": 3, "parabola_up": True}),
-    (QuadraticInequalityLessFactory, "x^2 - x - 2 < 0",
+    (q.QuadraticInequalityLessFactory, "x^2 - x - 2 < 0",
      {"exercise": "x^2 - x - 2 < 0", "solution_range": "(-1, 2)", "parabola_up": True}),
-    (QuadraticInequalityGreaterFactory, "x^2 - 3x + 2 > 0",
+    (q.QuadraticInequalityGreaterFactory, "x^2 - 3x + 2 > 0",
      {"exercise": "x^2 - 3x + 2 > 0", "solution_range": "(-∞, 1) U (2, ∞)", "parabola_up": True}),
 ])
 def test_quadratic_solve(factory_class, test_input, expected_output):
