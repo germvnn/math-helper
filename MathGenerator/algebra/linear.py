425262af13f3806790dc7a5347a36464bc8d0460
Daniel Morawski <gvrmvnn@gmail.com>
2024-04-22 07:51:52 +0200

    Implement base for SingleLinearFactory

--- MathGenerator/algebra/linear.py
+++ MathGenerator/algebra/linear.py
@@ -17,29 +17,45 @@ class LinearFactory(ExerciseFactory):
 class SingleLinearFactory(LinearFactory):
     """Base class for generating and solving single linear equations and inequalities"""
 
-    def __init__(self):
+    def __init__(self, operator_symbol: str | None = None):
         super().__init__()
+        self.operator = operator_symbol
+        self.random_operator = True if self.operator is None else False
 
 
 class SingleLinearEquationFactory(SingleLinearFactory):
     """Class for generating and solving single linear equations"""
 
     def __init__(self):
-        super().__init__()
+        super().__init__(operator_symbol="=")
 
 
 class SingleLinearInequalityLessFactory(SingleLinearFactory):
     """Class for generating and solving single linear inequalities with 'less than' condition"""
 
     def __init__(self):
-        super().__init__()
+        super().__init__(operator_symbol="<")
 
 
 class SingleLinearInequalityGreaterFactory(SingleLinearFactory):
     """Class for generating and solving single linear inequalities with 'greater than' condition"""
 
     def __init__(self):
-        super().__init__()
+        super().__init__(operator_symbol=">")
+
+
+class SingleLinearEqualLessFactory(SingleLinearFactory):
+    """Class for generating and solving single linear inequalities with 'greater than' condition"""
+
+    def __init__(self):
+        super().__init__(operator_symbol="<=")
+
+
+class SingleLinearEqualGreaterFactory(SingleLinearFactory):
+    """Class for generating and solving single linear inequalities with 'greater than' condition"""
+
+    def __init__(self):
+        super().__init__(operator_symbol=">=")
 
 
 class LinearSystemFactory(LinearFactory):
